b5c00c4eef4e5b76e7ebf738405e255a531d5979
mandicLuka <luka.mandic@fer.hr>
2026-08-20 13:45:33 +0200

    Update documentation

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="rpp_common",
     version="0.1.0",
     description="Common RPP plugin interfaces and description types",
-    packages=[
-        "rpp_common",
-        "rpp_common.common_plugins",
-    ],
-    package_dir={
-        "rpp_common": "rpp_common",
-        "rpp_common.common_plugins": "rpp_common/common_plugins",
-    },
+    packages=find_packages(include=["rpp_common", "rpp_common.*"]),
     include_package_data=True,
     install_requires=["numpy"],
 )
